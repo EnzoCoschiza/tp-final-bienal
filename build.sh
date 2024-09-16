032d547c4be99768824d0c2d1b04a6d9e921dc43
EnzoCoschiza <enzocoschiza15@gmail.com>
2024-09-16 20:33:04 -0300

    render 5

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r ./backend/requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
-python ./backend/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./backend/manage.py migrate
+python manage.py migrate
 
 # Create superuser using environment variables
-python ./backend/manage.py shell < ./backend/create_superuser.py
+python manage.py shell < create_superuser.py
